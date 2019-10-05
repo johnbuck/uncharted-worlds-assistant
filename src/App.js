628f4b10c102724d282c03c156ecfa070eef84dd
John Buckingham <john@pancakecreative.com>
2019-10-05 13:16:19 -0700

    Render added components

--- src/App.js
+++ src/App.js
@@ -1,26 +1,27 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React,  { Component } from 'react'
+import AppBar from '@material-ui/core/AppBar'
+//import NavBar from './components/NavBar'
+import SitRep from './components/SitRep'
+import Toolbar from '@material-ui/core/Toolbar'
+import TypoGraphy from '@material-ui/core/Typography'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+        <div className="App">
+          <AppBar color="primary" position="static">
+            <Toolbar>
+              <TypoGraphy variant="title"
+                          color="inherit"
+              >
+                Uncharted Worlds Assistant
+              </TypoGraphy>
+            </Toolbar>
+          </AppBar>
+          <SitRep spacing={40} />
+        </div>
+    );
+  }
 }
+export default App
 
-export default App;
