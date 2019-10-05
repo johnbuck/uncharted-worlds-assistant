894ff1f6d82355409f0abe0a8ac6eaff394b906c
John Buckingham <john@pancakecreative.com>
2019-10-05 09:00:36 -0700

    get relevant data from Airtable records

--- functions/functionName/functionName.js
+++ functions/functionName/functionName.js
@@ -3,60 +3,74 @@ const Airtable = require('airtable');
 
 /** THIS IS YOUR SERVERLESS FUNCTION */
 exports.handler = function(event, context, callback) {
-  //pull the required information from your environment variables, which can be set in the Netlify UI
-  const {API_URL, API_CLIENT_ID, API_KEY} = process.env;
+    //pull the required information from your environment variables, which can be set in the Netlify UI
+    const {API_URL, API_CLIENT_ID, API_KEY} = process.env;
 
-  // Return mock
-  if (!process.env.API_KEY) {
-    // eslint-disable-next-line no-console
-    console.log('No key: MOCK RESPONSE')
-    const response = {
-      statusCode: 200,
-      body: JSON.stringify(mockdata),
-      headers: {
-        'content-type': 'application/json',
-        'cache-control': 'Cache-Control: max-age=60, public'
-      }
-    }
-    return callback(null, response)
-  }
-
-  // CONFIGURE YOUR AIRTABLE BASE CONNECTION
-  Airtable.configure({
-    endpointUrl: API_URL,
-    apiKey: API_KEY
-  });
-  const base = Airtable.base(API_CLIENT_ID);
-
-  const allRecords = [];
-  base('Table 1')
-      .select({
-        maxRecords: 100,
-        view: 'All'
-      })
-      .eachPage(
-          function page(records, fetchNextPage) {
-            records.forEach(function (record) {
-              allRecords.push(record)
-            })
-            fetchNextPage()
-          },
-          function done(err) {
-            if (err) {
-              callback(err)
-            } else {
-              const body = JSON.stringify({records: allRecords})
-              const response = {
-                statusCode: 200,
-                body: body,
-                headers: {
-                  'content-type': 'application/json',
-                  'cache-control': 'Cache-Control: max-age=60, public'
-                }
-              }
-              callback(null, response)
+    // Return mock
+    if (!process.env.API_KEY) {
+        // eslint-disable-next-line no-console
+        console.log('No key: MOCK RESPONSE')
+        const response = {
+            statusCode: 200,
+            body: JSON.stringify(mockdata),
+            headers: {
+                'content-type': 'application/json',
+                'cache-control': 'Cache-Control: max-age=60, public'
             }
-          }
-      )
+        }
+    return callback(null, response)
+    }
 
+    // Configure Airtable Base Connection
+    Airtable.configure({
+        endpointUrl: API_URL,
+        apiKey: API_KEY
+    });
+
+    const base = Airtable.base(API_CLIENT_ID);
+
+    // Setup empty array to store results
+    const allRecords = [];
+
+    // Query
+    const apiQuery = {
+        maxRecords: 100,
+        view: 'All',
+        pageSize: 50,
+        // sort: [{field: 'Publish Date', direction: 'desc'}]
+    }
+
+    base('sitRep')
+        .select(apiQuery)
+        .eachPage((records, fetchNextPage) => {
+            records.forEach(function (record) {
+                const item = {
+                    key: record.get('key'),
+                    value: record.get('value'),
+                    type: record.get('type'),
+                    id: record.id
+                }
+
+                allRecords.push(item)
+            })
+
+            fetchNextPage()
+        },
+        function done(err) {
+            if (err) {
+                callback(err)
+            } else {
+                const body = JSON.stringify({records: allRecords})
+                const response = {
+                    statusCode: 200,
+                    body: body,
+                    headers: {
+                        'content-type': 'application/json',
+                        'cache-control': 'Cache-Control: max-age=60, public'
+                    }
+                }
+                callback(null, response)
+            }
+        })
 }
+
